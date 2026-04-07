5325d2a2bc5d610ab7082964797cc46dfa572c69
mioriaty <mioriaty@gmail.com>
2026-04-07 15:03:25 +0700

    update: audio question

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [
+    {
+      name: "E-Learning Platform",
+      script: "PORT=1112 npm run start",
+    },
+  ],
+};
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.join(process.cwd()),
+  },
+};
 
 export default nextConfig;
